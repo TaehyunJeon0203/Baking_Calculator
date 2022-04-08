c8dfb187d1c53accb4a2aeb13c13bd0bdc599aee
TaehyunJeon <jeontaehyun0203@gmail.com>
2022-04-09 01:13:49 +0900

    Style: test코드 삭제

--- js/tab.js
+++ js/tab.js
@@ -2,20 +2,14 @@ document.querySelector('#tabBtn').addEventListener('click', clickBtn);
 
 function clickBtn(e) {
 	e.preventDefault;
-	console.log(e.target);
 	
 	let tab_id = e.target.getAttribute('data-tab');
 
 	const tabsGroup = document.querySelectorAll('.tab-link');
 	const tabContentGroup = document.querySelectorAll('.tab-content');
-	console.log(tabsGroup);
 
-	tabsGroup.forEach(function(e) {
-		e.classList.remove('current');
-	})
-	tabContentGroup.forEach(function(e) {
-		e.classList.remove('current');
-	})
+	tabsGroup.forEach(element => element.classList.remove('current'));
+	tabContentGroup.forEach(element => element.classList.remove('current'));
 
 	e.target.classList.add('current');
 	document.querySelector("#" + tab_id).classList.add('current');
